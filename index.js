6ad5f53fa5eebb36d444dff8200a8b3e967ccf5f
ebwinters@comcast.net <ebwinters@comcast.net>
2020-03-17 14:07:18 -0400

    decide API and call with it

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 #!/usr/bin/env node
+const fetch = require("node-fetch");
 console.log("Here are the latest Corona Virus stats, courtesy of Worldometers:");
 console.log(process.argv);
+fetch('https://corona.lmao.ninja/countries/USA')
+	.then((response) => {
+    	return response.json();
+  	})
+  	.then((data) => {
+    	console.log(data);
+  	});
