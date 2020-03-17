0dc9b492c389c23e75801b0e4a9abf58d1b40d2e
ebwinters@comcast.net <ebwinters@comcast.net>
2020-03-17 14:15:35 -0400

    working with API

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 const fetch = require("node-fetch");
+const table = require('cli-table');
 console.log("Here are the latest Corona Virus stats, courtesy of Worldometers:");
 console.log(process.argv);
 fetch('https://corona.lmao.ninja/countries/USA')
@@ -7,5 +8,13 @@ fetch('https://corona.lmao.ninja/countries/USA')
     	return response.json();
   	})
   	.then((data) => {
-    	console.log(data);
+    	console.log(data.country + " data:");
+		var t = new table({
+		    head: ['Cases', 'Deaths'],
+			colWidths: [100, 200]
+		});
+		t.push(
+			[data.cases, data.deaths],
+		);
+		console.log(t.toString());
   	});
