8359d74cf292ea242f205a9550f50d337955cf72
ebwinters@comcast.net <ebwinters@comcast.net>
2020-06-11 22:22:21 -0400

    update URLs

--- apiCalls.js
+++ apiCalls.js
@@ -1,38 +1,38 @@
 const fetch = require("node-fetch");
 
 async function processAllData() {
-    const response = await fetch(`https://corona.lmao.ninja/all`);
+    const response = await fetch(`https://disease.sh/v2/all`);
     const data = await response.json();
     data.country = "general";
     return data;
 }
 
 async function processCountryData(arg) {
-	const response = await fetch(`https://corona.lmao.ninja/countries/${arg}`);
+	const response = await fetch(`https://disease.sh/v2/countries/${arg}`);
 	const data = await response.json();
 	return data;
 }
 
 async function processAllCountries() {
-	const response = await fetch(`https://corona.lmao.ninja/countries`);
+	const response = await fetch(`https://disease.sh/v2/countries`);
 	const data = await response.json();
 	return data;
 }
 
 async function processAllStates() {
-	const response = await fetch(`https://corona.lmao.ninja/states`);
+	const response = await fetch(`https://disease.sh/v2/states`);
 	const data = await response.json();
 	return data;
 }
 
 async function processAllHistoricalData() {
-	const response = await fetch(`https://corona.lmao.ninja/v2/historical`);
+	const response = await fetch(`https://disease.sh/v2/v2/historical`);
 	const data = await response.json();
 	return data;
 }
 
 async function processCountryHistoricalData(arg) {
-	const response = await fetch(`https://corona.lmao.ninja/v2/historical/${arg}`);
+	const response = await fetch(`https://disease.sh/v2/v2/historical/${arg}`);
 	const data = await response.json();
 	return data;
 }
